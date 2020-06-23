f55aa28e8ce6708f1a08a40b974a9e8ca41dc077
LaupWing <loc_strict@hotmail.com>
2020-06-23 18:20:30 +0200

    10

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const {app, BrowserWindow} = require('electron');
 process.env.NODE_ENV = 'development';
 
 const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isMac = process.platform === 'darwin' ? true : false;
 
 let mainWindow
 
@@ -11,10 +12,22 @@ function createMainWindow(){
         title: 'ImageShrink',
         width: 500,
         height: 600,
-        icon: './assets/icons/Icon_256x256.png'
+        icon: './assets/icons/Icon_256x256.png',
+        resizable: isDev
     });
 
     mainWindow.loadFile('./app/index.html');
 }
 
 app.on('ready', createMainWindow);
+
+app.on('window-all-closed', ()=>{
+    if(!isMac){
+        app.quit();
+    }
+});
+app.on('activate', ()=>{
+    if(BrowserWindow.getAllWindows().length === 0){
+        createMainWindow();
+    }
+});
